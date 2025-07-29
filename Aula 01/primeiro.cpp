8cef6509cbd8bb06e587a76162d8158044008cfb
Lucas Laurent <108150568+lucaslaurent@users.noreply.github.com>
2025-07-29 10:46:31 -0300

    Add files via upload

---
+++ Aula 01/primeiro.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, world!\n";
+    std::cout << "Hey, you!" << std::endl;
+    std::cout << "Demi lovato is the new princess of pop.";
+
+    return 0;
+}
